6b298dad4e47a425a1065d0d42b2de4c65c0fbdf
aftab-08khan <akkhanaftab08@gmail.com>
2026-01-12 13:38:15 +0400

    Problem 2 : Find Vowels

--- index.js
+++ index.js
@@ -10,12 +10,40 @@
 
 // Second Approach
 
-function findPallindrome2(text) {
-  let reverseText2 = "";
-  for (let i = text.length - 1; i >= 0; i--) {
-    reverseText2 += text[i];
-  }
+// function findPallindrome2(text) {
+//   let reverseText2 = "";
+//   for (let i = text.length - 1; i >= 0; i--) {
+//     reverseText2 += text[i];
+//   }
 
-  return reverseText2 === text;
+//   return reverseText2 === text;
+// }
+// console.log(findPallindrome2("madam"));
+
+// 02) Find Vowels
+
+//  First Approach
+
+function uniqueNumbers(arr) {
+  let res = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (!res.includes(arr[i])) {
+      res.push(arr[i]);
+    }
+  }
+  return res;
 }
-console.log(findPallindrome2("madam"));
+function findVowles(text) {
+  let vowels = ["a", "e", "i", "o", "u"];
+  let result = [];
+
+  for (let i = 0; i < text.length; i++) {
+    if (vowels.includes(text[i])) {
+      result.push(text[i]);
+    }
+  }
+  const finalRes = uniqueNumbers(result);
+
+  return finalRes;
+}
+console.log(findVowles("aftab ee khan"));
