08bdee45057f89c5b1d2eccde98b7bac799df21d
aftab-08khan <akkhanaftab08@gmail.com>
2026-07-06 14:34:37 +0400

    Problem 4 : Find TWOSUM from an Array

--- index.js
+++ index.js
@@ -66,32 +66,46 @@
 
 // console.log(missingNumbers([1, 2, 3, 5, 7, 9]));
 
-
-
 // 04) Move All Zero At the End
 
 // Frist Approach
 
+// const arr = [0,-43,34545,4545,6,6,0,244,0,9]
+// function moveZeros(numbers){
+//    let res = []
 
-const arr = [0,-43,34545,4545,6,6,0,244,0,9]
-function moveZeros(numbers){
-   let res = []
+//    for(let i = 0 ; i < numbers.length ; i++){
+//     if(numbers[i] === 0){
+//         res.push(numbers[i])
+//     }else{
+//       res.unshift(numbers[i])
+//     }
+//    }
 
+//    return res
 
-   for(let i = 0 ; i < numbers.length ; i++){
-    if(numbers[i] === 0){
-        res.push(numbers[i])
-    }else{
-      res.unshift(numbers[i])  
+// }
+
+// console.log(moveZeros(arr));
+
+// 05) FInd ToSum from An Array
+
+// First Approach
+const arr = [2, 3, 4, 5, 6, 7, 8, 9];
+
+function toSum(numbers, target) {
+  let res = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+        
+      if (numbers[i] + numbers[j] === target) {
+        
+        res.push([numbers[i] , numbers[j] ]);
+      }
     }
-   }
-
-   return res
-
+  }
+  return res;
 }
 
-
-
-
-console.log(moveZeros(arr));
-
+console.log(toSum(arr, 12));
